036699ecb26b5ba41fddb6aa652da5a2bd949e40
Dung <dungtrinh0702@gmail.coom>
2020-03-02 21:40:13 +0700

    update

--- DemergingUseQueue/src/DemergingTest.java
+++
@@ -1,9 +0,0 @@
-import java.text.ParseException;
-
-public class DemergingTest {
-    public static void main(String[] args) throws ParseException {
-        StoreInformation<DemergingTest> store = new StoreInformation<>();
-        store.add();
-        store.addSexToQueue();
-    }
-}
